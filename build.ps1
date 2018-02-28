717bb9124390b1f89728d270c19ec35bd31a12ee
Maximilian Krüger <maximilian.krueger@it.konicaminolta.de>
2018-02-28 13:59:33 +0100

    create new build script

---
+++ build.ps1
@@ -0,0 +1,56 @@
+"Start building"
+function StartProcess($target, $argument) {
+    $result = [System.Diagnostics.Process]::Start($target, $argument)
+
+    $result.WaitForExit()
+    if($result.ExitCode -ne 0)
+    {
+        $result.ExitCode
+        exit
+    }
+}
+
+$plgx = "KeePassHttp.plgx"
+
+if([System.IO.File]::Exists($plgx)){
+    "Remove old KeePassHttp.plgs"
+    Remove-Item $plgx
+}
+
+$mono = "mono\KeePassHttp.dll"
+if([System.IO.File]::Exists($mono)){
+    "Remove old mono\KeePassHttp.dll"
+    Remove-Item $mono
+}
+
+$KeePass = (get-item env:"ProgramFiles(x86)").Value + "\KeePass Password Safe 2\KeePass.exe"
+$base = $PSScriptRoot + "\"
+
+"Create plgx"
+
+StartProcess  -target $KeePass -argument $("--plgx-create `"" + $base + "KeePassHttp`"")
+
+
+"Create finished"
+
+$msbuild = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Enterprise\MSBuild\15.0\Bin\MSBuild.exe"
+
+"Clean KeePassHttp.sln"
+
+StartProcess  -target $msbuild -argument $("/target:clean `"" + $base + "KeePassHttp.sln`"")
+
+"Clean finished"
+
+"Build Release KeePassHttp"
+
+StartProcess  -target $msbuild -argument $("/p:Configuration=Release `""+ $base +  "KeePassHttp.sln`"")
+
+"Build finished"
+
+"Copy KeePassHttp.dll to mono"
+$path = "" + $base + "KeePassHttp\bin\Release\KeePassHttp.dll"
+$destionation = "" + $base + "mono\"
+Copy-Item -Path $path -Destination $destionation
+
+"Building finished"
+
